7376c5abde794e1c2f36d6db90ef572083dfe3d6
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2024-01-24 07:47:29 -0600

    Ejercicio1 de Web

---
+++ src/web/ejercicio1.java
@@ -0,0 +1,13 @@
+package web;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        int numberEjercicio1 = 12;
+        int numberEjercicio2 = 5;
+
+        System.out.println("La suma de los números dados es: " + (numberEjercicio1 + numberEjercicio2));
+        System.out.println("La resta de los números dados es: " + (numberEjercicio1 - numberEjercicio2));
+        System.out.println("La multiplicación de los números dados es: " + (numberEjercicio1 * numberEjercicio2));
+        System.out.println("La suma de los division dados es: " + (numberEjercicio1 / numberEjercicio2));
+    }
+}
