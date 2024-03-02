7b20b230bab0663d7bc8051c39216131017eea9c
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2024-03-01 21:01:36 -0600

    Ejercicio

---
+++ src/GoIt/VariablesYTipos/Ejercicio1.java
@@ -0,0 +1,18 @@
+package GoIt.VariablesYTipos;
+
+public class Ejercicio1 {
+
+    /*
+    *Declare dos variables, productName para el nombre del producto y pricePerItem para almacenar el precio por artículo. Al declarar las variables, se debe asignar las siguientes características del producto a las variables:
+
+    título - línea "Droid"
+    precio por unidad - número 2000
+    * */
+    public static void main(String[] args) {
+        String productName = "Droid";
+        int pricePerItem = 2000;
+
+        System.out.println(productName);
+        System.out.println(pricePerItem);
+    }
+}
